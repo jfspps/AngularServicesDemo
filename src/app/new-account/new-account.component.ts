6f68c7c9595c003465a95150c442dcff6956a603
jfspps <jfsapps@gmail.com>
2021-01-13 15:25:21 +0000

    replaced duplicate code with a Service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,26 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../login.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]   //needed to inject LoggingService
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService) {
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+
+    // console.log('A server status changed, new status: ' + accountStatus);
+    // using services to minimise code duplication (see also account component)
+    this.loggingService.logStatusChanged(accountStatus);
   }
 }
